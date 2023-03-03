c3ec84c369d60555c1fa0c6e0cb502075516a9bf
Hootan Shadmehr <hootan@umich.edu>
2023-03-03 01:56:20 -0800

    Add vga_buffer module

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,43 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Hello, World!";
 
 #[no_mangle]
 /// This function is the entry point, since the linker looks for a function named `_start` by default
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+
+    // vga_buffer::print_something();
+
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(
+    //     vga_buffer::WRITER.lock(),
+    //     ", some numbers: {} {}",
+    //     42,
+    //     1.337
+    // )
+    // .unwrap();
+
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
